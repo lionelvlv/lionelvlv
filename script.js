0d6e62b71858a708cf6018e58e427edc60cb537b
Lionel Verano <lionel.verano.iv@berkeley.edu>
2024-02-13 20:56:21 -0800

    stuff

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+document.getElementById('yesBtn').addEventListener('click', function() {
+  document.getElementById('valentinePrompt').style.display = 'none';
+  const responseDiv = document.getElementById('response');
+  responseDiv.innerHTML = '<p>Yay!!! See you on Feb 14th</p><img src="https://i.pinimg.com/originals/50/85/26/508526db68210f54e035517c1f3d6dd3.gif" alt="Celebratory Image">';
+  responseDiv.style.display = 'block';
+});
+
+document.getElementById('noBtn').addEventListener('mouseover', function() {
+  this.style.position = 'absolute';
+  this.style.top = Math.random() * window.innerHeight + 'px';
+  this.style.left = Math.random() * window.innerWidth + 'px';
+});
